0b826cfd2592c8129b22c88388ef552bc2c2720a
kekskn <knezevicksenija42@gmail.com>
2022-10-13 17:43:10 +0300

    init

---
+++ auth/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('register', views.register_request, name="register"),
+    path('login', views.login_request, name="login"),
+    path('logout', views.logout_request, name="logout"),
+
+]
---
+++ tripoholics/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'stmp.gmail.com'
+EMAIL_HOST_USER = 'ksenknezevic@gmail.com'
+EMAIL_HOST_PASSWORD = 'ksenijakeksi123'
+EMAIL_PORT = 587
